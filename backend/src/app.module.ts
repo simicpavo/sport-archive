3d98bcb48641d356f47c41b4013f6e2a2b714eaa
simicpavo <pavsimic@gmail.com>
2025-09-15 01:15:56 +0200

    Merge branch 'main' of https://github.com/simicpavo/sport-archive into feat/25-add-mediaNews-and-mediaSource-endpoints

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CronModule } from './chron/chron.module';
@@ -15,6 +16,7 @@ import { SportsModule } from './sports/sports.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ isGlobal: true, envFilePath: '.env' }),
     PrismaModule,
     SportsModule,
     PersonsModule,
