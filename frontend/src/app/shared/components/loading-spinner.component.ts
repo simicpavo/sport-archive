af06d450dab8461688937d5e670b1336dfe0c03f
simicpavo <pavsimic@gmail.com>
2025-10-19 21:36:41 +0200

    Add custom reusable component

---
+++ frontend/src/app/shared/components/loading-spinner.component.ts
@@ -0,0 +1,16 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { ProgressSpinnerModule } from 'primeng/progressspinner';
+
+@Component({
+  selector: 'app-loading-spinner',
+  standalone: true,
+  imports: [CommonModule, ProgressSpinnerModule],
+  template: `<div class="flex justify-content-center mb-4">
+    <p-progressSpinner
+      strokeWidth="4"
+      [style]="{ width: '50px', height: '50px' }"
+    ></p-progressSpinner>
+  </div>`,
+})
+export class LoadingSpinnerComponent {}
