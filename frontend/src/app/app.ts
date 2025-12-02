abc329732fbd379900e0f9d76ff82108cb867302
simicpavo <pavsimic@gmail.com>
2025-12-02 12:44:48 +0100

    Update Toast import to ToastModule

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,10 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Toast } from 'primeng/toast';
+import { ToastModule } from 'primeng/toast';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Toast],
+  imports: [RouterOutlet, ToastModule],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
