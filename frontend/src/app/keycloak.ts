50b192dba6524b7c7b0475201f9684dd12f7d85e
ilijaleko <ilija.leko.86@gmail.com>
2026-03-23 17:33:10 +0100

    Add new deployment config

--- frontend/src/app/keycloak.ts
+++ frontend/src/app/keycloak.ts
@@ -1,7 +1,8 @@
 import Keycloak from 'keycloak-js';
+import { environment } from '../environments/environment';
 
 export const keycloak = new Keycloak({
-  url: 'http://localhost:8081',
+  url: environment.keycloakUrl,
   realm: 'sport-archive',
   clientId: 'sport-archive',
 });
--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
   apiUrl: 'API_URL',
+  keycloakUrl: 'KEYCLOAK_URL',
 };
